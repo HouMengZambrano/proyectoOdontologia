1798c483d45ec40d4f9ae2ad86c467cd58ef64e9
HouMengZambrano <houmeng.zambrano@gmail.com>
2024-08-15 15:48:28 -0300

    Styles added to about me page

--- odontologia_marilian_gonzalez/src/SobreMi.tsx
+++ odontologia_marilian_gonzalez/src/SobreMi.tsx
@@ -11,11 +11,11 @@ export const SobreMi = () => {
     <div className='sobreMi'>
     <div className ='sobreMiArticle'>
       <img src={sobreMiImg} alt ="sobremiImg"></img>
-        <h3>Odontologa Marilian Gonzales</h3>
-        <h3>Especialista en Ortodoncia y Estetica dental</h3>
-        <p>
-        "Graduada de la Universidad de los Andes en Venezuela, mi compromiso con la excelencia en odontología ha sido una constante en mi carrera. Me especialicé en cirugía bucal y ortodoncia en la misma universidad, y luego continué mi formación en estética dental en la Fundación Creo de la Provincia de Córdoba, Argentina. He dedicado mi vida profesional a perfeccionar mis habilidades para ofrecerte los mejores tratamientos dentales. Actualmente, me encuentro realizando una especialización en implantes y rehabilitación dental en el prestigioso Centro Odontológico Dr. Gustavo Chiterer en la Ciudad Autónoma de Buenos Aires."
-        </p>
+      <h3>Odontologa Marilian Gonzales</h3>
+      <h4>Especialista en Ortodoncia y Estetica dental</h4>
+      <p>
+      "Graduada de la Universidad de los Andes en Venezuela, mi compromiso con la excelencia en odontología ha sido una constante en mi carrera. Me especialicé en cirugía bucal y ortodoncia en la misma universidad, y luego continué mi formación en estética dental en la "Fundación Creo" de la Provincia de Córdoba, Argentina. He dedicado mi vida profesional a perfeccionar mis habilidades para ofrecerte los mejores tratamientos dentales. Actualmente, me encuentro realizando una especialización en implantes y rehabilitación dental en el prestigioso Centro Odontológico Dr. Gustavo Chiterer en la Ciudad Autónoma de Buenos Aires."
+      </p>
     </div>
     </div>
     <Footer/>
