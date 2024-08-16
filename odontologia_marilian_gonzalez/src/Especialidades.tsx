43d4566ac69d0e7fd6ca935b7ec56a74341caba4
HouMengZambrano <houmeng.zambrano@gmail.com>
2024-08-16 13:15:22 -0300

    scrooling to especialidad added and scroll top tomponent added in the routes

--- odontologia_marilian_gonzalez/src/Especialidades.tsx
+++ odontologia_marilian_gonzalez/src/Especialidades.tsx
@@ -1,67 +1,108 @@
-// import React from 'react'
-import Article from './components/Article'
-import TarjetaEspecialidad from './components/TarjetaEspecialidad'
-import imgEspecialidad from './assets/imagenEspecialidad.png'
-import Footer from './components/Footer'
-import Header from './components/Header'
-import { useEffect, useRef } from 'react'
-import { useLocation } from 'react-router-dom'
+import React, { useEffect, useRef } from 'react';
+import { useLocation } from 'react-router-dom';
+import Article from './components/Article';
+import TarjetaEspecialidad from './components/TarjetaEspecialidad';
+import imgEspecialidad from './assets/imagenEspecialidad.png';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import './sytles/Especialidades.css'
 
+const articlesData = [
+  {
+    id: "1",
+    nombre: "Odontología General",
+    descripcion: "Nuestros servicios incluyen limpieza dental profesional, rehabilitación oral, resinas estéticas, y tratamientos de caries",
+    imagenUrl: imgEspecialidad,
+  },
+  {
+    id: "2",
+    nombre: "Ortodoncia y Ortopedia",
+    descripcion: "Nuestros servicios incluyen el diagnóstico y corrección de la posición de los dientes y la mandíbula en niños y adultos, garantizando una sonrisa alineada, funcional y saludable",
+    imagenUrl: imgEspecialidad,
+  },
+  {
+    id: "3",
+    nombre: "Cirugía Bucal",
+    descripcion: "Nuestros servicios incluyen la extracción de piezas dentales, retiro de lesiones, corrección de fracturas, y cirugía ortognática, cuidando de tus dientes, mucosa y tejidos accesorios.",
+    imagenUrl: imgEspecialidad,
+  },
+  {
+    id: "4",
+    nombre: "Estética",
+    descripcion: "Nuestros tratamientos mejoran la apariencia de tu sonrisa, desde blanqueamientos dentales hasta diseños de sonrisa con carillas de composite. Modificamos la forma, color y tamaño de los dientes para armonizarlos con tus encías y labios.",
+    imagenUrl: imgEspecialidad,
+  },
+  {
+    id: "5",
+    nombre: "Rehabilitación e Implantología",
+    descripcion: "Ofrecemos un tratamiento quirúrgico diseñado para reemplazar dientes perdidos mediante implantes de titanio que sustituyen la raíz ausente, seguido del diseño de coronas dentales para adaptarse a la nueva raíz, restaurando la función y estética de tu sonrisa",
+    imagenUrl: imgEspecialidad,
+  },
+  {
+    id: "6",
+    nombre: "Odontopediatría",
+    descripcion: "En nuestras consultas, nos enfocamos en atender a todos los niños con cuidado y dedicación. Creamos un ambiente amigable y acogedor para que cada visita sea una experiencia positiva, independientemente de las necesidades específicas de cada niño. Nuestro equipo está comprometido en hacer que todos los niños se sientan cómodos y seguros durante su atención dental.",
+    imagenUrl: imgEspecialidad,
+  },
+  {
+    id: "7",
+    nombre: "Endodoncia",
+    descripcion: "Nuestros tratamientos de endodoncia se enfocan en el cuidado de la pulpa dental y los conductos, asegurando la salud y el bienestar de tus dientes desde adentro hacia afuera.",
+    imagenUrl: imgEspecialidad,
+  },
+];
 
 const Especialidad = () => {
-  const location = useLocation()
-  const query = new URLSearchParams(location.search)
-  const especialidad = query.get('id')
+  const location = useLocation();
+  const query = new URLSearchParams(location.search);
+  const especialidad = query.get('id');
 
-  const especialidadRef = useRef<HTMLDivElement>(null)
+  // Create refs for each article
+  const refs = useRef(articlesData.reduce((acc, value) => {
+    acc[value.id] = React.createRef<HTMLDivElement>();
+    return acc;
+  }, {} as { [key: string]: React.RefObject<HTMLDivElement> }));
 
-  const scrollToEspecialidad = (id: string | null) =>{
-    switch(id){
-      case '7':
-        especialidadRef.current?.scrollIntoView({
-          behavior: 'smooth'
+  // Scroll to the corresponding article based on the id from the query
+  const scrollToEspecialidad = (id: string | null) => {
+    if (id && refs.current[id]) {
+      setTimeout(() => {
+        refs.current[id].current?.scrollIntoView({
+          behavior: 'smooth',
+          block: 'center',
         });
-        break;
-      default:
-        break;
+      }, 100);
     }
-  }
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
+    window.scrollTo(0,0)
     scrollToEspecialidad(especialidad);
-  }, [especialidad])
+  }, [especialidad]);
+
+  const renderArticles = (articles: typeof articlesData) => {
+    return articles.map((article) => (
+      <TarjetaEspecialidad
+        key={article.id}
+        id={article.id}
+        nombre={article.nombre}
+        descripcion={article.descripcion}
+        imagenUrl={article.imagenUrl}
+        ref={refs.current[article.id]} // Attach the corresponding ref to each article
+      />
+    ));
+  };
 
   return (
     <div>
-        <Header/>
-        <Article title='Especialidades' textContent= 'Descubre Nuestras Áreas de Especialización'/>
-        <div className='contenedorEspecialidades'>
-          <TarjetaEspecialidad id="1" nombre="Odontología General" descripcion='Nuestros servicios incluyen limpieza dental profesional, rehabilitación oral, resinas estéticas, y tratamientos de caries'
-          imagenUrl={imgEspecialidad}
-          />
-          <TarjetaEspecialidad id="2" nombre="Ortodoncia y Ortopedia" descripcion='Nuestros servicios incluyen el diagnóstico y corrección de la posición de los dientes y la mandíbula en niños y adultos, garantizando una sonrisa alineada, funcional y saludable'
-          imagenUrl={imgEspecialidad}
-          />
-          <TarjetaEspecialidad id="3" nombre="Cirugía Bucal" descripcion='Nuestros servicios incluyen la extracción de piezas dentales, retiro de lesiones, corrección de fracturas, y cirugía ortognática, cuidando de tus dientes, mucosa y tejidos accesorios.'
-          imagenUrl={imgEspecialidad}
-          />
-          <TarjetaEspecialidad id="4" nombre="Estética" descripcion='Nuestros tratamientos mejoran la apariencia de tu sonrisa, desde blanqueamientos dentales hasta diseños de sonrisa con carillas de composite. Modificamos la forma, color y tamaño de los dientes para armonizarlos con tus encías y labios.'
-          imagenUrl={imgEspecialidad}
-          />
-          <TarjetaEspecialidad id="5" nombre="Rehabilitación e Implantología" descripcion='Ofrecemos un tratamiento quirúrgico diseñado para reemplazar dientes perdidos mediante implantes de titanio que sustituyen la raíz ausente, seguido del diseño de coronas dentales para adaptarse a la nueva raíz, restaurando la función y estética de tu sonrisa'
-          imagenUrl={imgEspecialidad}
-          />
-          <TarjetaEspecialidad id="6" nombre="Odontopediatría" descripcion='En nuestras consultas, nos enfocamos en atender a todos los niños con cuidado y dedicación. Creamos un ambiente amigable y acogedor para que cada visita sea una experiencia positiva, independientemente de las necesidades específicas de cada niño. Nuestro equipo está comprometido en hacer que todos los niños se sientan cómodos y seguros durante su atención dental. '
-          imagenUrl={imgEspecialidad}
-          />
-          <TarjetaEspecialidad id="7" nombre="Endodoncia" descripcion='Nuestros tratamientos de endodoncia se enfocan en el cuidado de la pulpa dental y los conductos, asegurando la salud y el bienestar de tus dientes desde adentro hacia afuera. '
-          imagenUrl={imgEspecialidad}
-          ref={especialidadRef}
-          />
-        </div>
-        <Footer/>
+      <Header />
+      <Article title="Especialidades" textContent="Descubre Nuestras Áreas de Especialización" />
+      <div className="contenedorEspecialidades">
+        {renderArticles(articlesData)}
+      </div>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Especialidad
+export default Especialidad;
--- odontologia_marilian_gonzalez/src/main.tsx
+++ odontologia_marilian_gonzalez/src/main.tsx
@@ -6,18 +6,21 @@ import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Contacto from './Contacto.tsx'
 import Especialidad from './Especialidades.tsx'
 import { SobreMi } from './SobreMi.tsx'
+import ScrollToTop from './components/ScrollToTop.tsx'
 
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
+    <ScrollToTop>
       <Routes>
           <Route path="/" element={<App/>}/>
           <Route path="/contacto" element={<Contacto/>}/>
           <Route path="/especialidad" element={<Especialidad/>}/>
           <Route path="/sobremi" element={<SobreMi/>}/>
       </Routes>
+    </ScrollToTop>
     </BrowserRouter>
   </React.StrictMode>,
 )
